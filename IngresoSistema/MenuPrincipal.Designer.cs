ee97e49e0516bcead04c4bbb2e3487bce95999d2
matiiezequiiel <matii.ezequiiel@gmail.com>
2020-09-30 12:48:20 -0300

    Parcial_1

--- IngresoSistema/MenuPrincipal.Designer.cs
+++ IngresoSistema/MenuPrincipal.Designer.cs
@@ -316,6 +316,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MenuPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MenuPrincipal";
             this.Load += new System.EventHandler(this.MenuPrincipal_Load);
             this.menuStrip1.ResumeLayout(false);
